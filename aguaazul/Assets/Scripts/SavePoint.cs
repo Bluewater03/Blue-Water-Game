995215b1a863254c851372fa114d523a8ff49d58
Bluewater03 <aguamaisazul@gmail.com>
2022-05-31 16:50:09 -0300

    atualização do projeto

---
+++ aguaazul/Assets/Scripts/SavePoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI; //sem isso a inteface nao funciona
+using UnityEngine.SceneManagement; //bliblioteca pra gereniar a cena
+
+public class SavePoint : MonoBehaviour
+{
+    public int totalPontos;
+    public Text contador;
+    
+    public GameObject gameOver;
+    
+    public static SavePoint acesso;
+
+    void Start()
+    {
+        acesso = this;
+    
+    }
+
+    public void atualizaPontosText() => contador.text = totalPontos.
+    ToString();
+
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+
+   public void RestartGame(string level)
+   {
+     SceneManager.LoadScene(level);
+   }
+
+}
